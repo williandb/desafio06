f70b85d96f0942c829dfd54992b10764ebd993ac
Willian Dagostin Bittencourt <williandb@gmail.com>
2021-08-05 20:02:02 -0300

    texto

--- src/service/ServicoService.java
+++ src/service/ServicoService.java
@@ -3,16 +3,16 @@ package service;
 public class ServicoService implements Persistivel{
     @Override
     public void save() {
-
+        System.out.println("Salvando");
     }
 
     @Override
     public void update() {
-
+        System.out.println("Atualizando");
     }
 
     @Override
     public void remove() {
-
+        System.out.println("Excluindo");
     }
 }
